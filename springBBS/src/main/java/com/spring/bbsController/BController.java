c5231bfcc75e18fc05ed1c2e11b296b902812b47
LCL <crforest0301@gmail.com>
2017-11-27 22:04:57 +0900

    Add a new project to learn Spring concepts (8)
    1) springII_Ex04:
    
    To learn validation function using validator
    
    Google formatter not applied
    Javadoc not applied

--- springBBS/src/main/java/com/spring/bbsController/BController.java
+++ springBBS/src/main/java/com/spring/bbsController/BController.java
@@ -1,13 +1,10 @@
 package com.spring.bbsController;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.bbsCommand.Bcmd;
 
---
+++ springII_Ex04/src/main/java/com/spring/validationEx/MemberValidator.java
@@ -0,0 +1,40 @@
+package com.spring.validationEx;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class MemberValidator implements Validator{
+	
+	@Override
+	public boolean supports(Class<?> arg0){
+		return Member.class.isAssignableFrom(arg0);
+	}
+
+  @Override
+  public void validate(Object obj, Errors errors) { 
+	  	//MEMO mValidator.validate(member, result);
+	  	//QUESTION なんでパラメーター名が違っても作動？
+	  
+	  System.out.println("-----validate()呼ぶ-----");
+	  Member member=(Member)obj; //QUESTION ()って？
+	  
+	  String memName=member.getName();
+	  if(memName==null||memName.trim().isEmpty()){
+		  System.out.println("会員の名前を入力しなさい");
+		  errors.rejectValue("name", "入力時エラー発生");
+	  }
+	  
+	  String memId=member.getId();
+	  if(memId==null||memId.trim().isEmpty()){
+		  System.out.println("会員のIDを入力しなさい");
+		  errors.rejectValue("id", "入力時エラー発生");
+	  }
+	  
+	  int memNo=member.getMemNo();
+	  if(memNo==0){
+		  System.out.println("マイナンバーを入力しなさい");
+		  errors.rejectValue("memNo", "入力時エラー発生");
+	  }	  
+	  
+  }
+}
