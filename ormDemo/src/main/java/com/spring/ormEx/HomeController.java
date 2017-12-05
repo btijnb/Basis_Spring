c4f77a9b03e39d3c0bb406e8704c75fad9e31352
LCL <crforest0301@gmail.com>
2017-12-05 10:40:59 +0900

    Apply MyBatis

--- ormDemo/src/main/java/com/spring/ormEx/HomeController.java
+++ ormDemo/src/main/java/com/spring/ormEx/HomeController.java
@@ -1,16 +1,14 @@
 package com.spring.ormEx;
 
-import java.util.ArrayList;
 
-
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.ormDAO.BDAO;
 import com.spring.ormDAO.BbsDAO;
-import com.spring.ormVO.BVO;
 
 @Controller
 public class HomeController {
@@ -18,12 +16,23 @@ public class HomeController {
 	@Autowired
 	BbsDAO bbsdao;
 	
-	
+	@Autowired
+	private SqlSession sqlSession;
+		//MEMO SqlSessionTemplateはSqlSession interfaceをimplement
+		//MEMO Type hierarchyはCommand + Tで確認
+
 	@RequestMapping("/list")
 	public String list(Model model){
-		ArrayList<BVO> bvos=bbsdao.list();
-		model.addAttribute("list", bvos);
+		BDAO bdao = sqlSession.getMapper(BDAO.class);
+		model.addAttribute("list", bdao.list());
 		return "/list";
-	}
+	}	
+	
+//	@RequestMapping("/list")
+//	public String list(Model model){
+//		ArrayList<BVO> bvos=bbsdao.list();
+//		model.addAttribute("list", bvos);
+//		return "/list";
+//	}
 	
 }
